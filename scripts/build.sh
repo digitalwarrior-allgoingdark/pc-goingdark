aa3d1b8d3f1712b1bee6d5fb64d44829e79bc168
DigitalWarrior <digitalwarrior@allgoingdark.com>
2012-05-09 22:27:07 -0400

    working out build bugs

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 
 sudo $WORKSPACE/scripts/prepare_isobuilder.sh
 mkdir $WORKSPACE/isoworking
-sudo /usr/bin/uck-remaster -m lubuntu-12.04-desktop-i386.iso $WORKSPACE/scripts/uck_scripts $WORKSPACE/isoworking
+sudo /usr/bin/uck-remaster -m $WORKSPACE/lubuntu-12.04-desktop-i386.iso $WORKSPACE/scripts/uck_scripts $WORKSPACE/isoworking
 
