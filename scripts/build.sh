09d7c5a1845504aafe46e7e7891e9c31c5ddd9c8
digitalwarrior <digitalwarrior@allgoingdark.com>
2012-05-09 21:33:56 -0300

    working the uck-remaster command

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 
 sudo $WORKSPACE/scripts/prepare_isobuilder.sh
 mkdir $WORKSPACE/remaster-iso
-sudo /usr/bin/uck-remaster lubuntu-12.04-desktop-i386.iso $WORKSPACE/scripts/uck_scripts/
+sudo /usr/bin/uck-remaster -m lubuntu-12.04-desktop-i386.iso $WORKSPACE/scripts/uck_scripts/ $WORKSPACE
 
